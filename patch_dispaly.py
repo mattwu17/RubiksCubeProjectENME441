ea7d914d3a24c3dc6b3a8e49c612b5f2729af425
StevenW1009 <69173205+StevenW1009@users.noreply.github.com>
2022-11-15 01:20:08 -0500

    Add files via upload
    This is a display option using a "patchwork" of cells to form a cube. This has potential to work.

---
+++ patch_dispaly.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import product, combinations
+from matplotlib.patches import Rectangle
+import mpl_toolkits.mplot3d.art3d as art3d
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+
+
+r = [-3, 3]
+for s, e in combinations(np.array(list(product(r,r,r))), 2):
+    if np.sum(np.abs(s-e)) == r[1]-r[0]:
+        ax.plot3D(*zip(s,e), color="black")
+
+colors = ['b', 'g', 'r', 'c', 'm', 'y']
+for i, (z, zdir) in enumerate(product([-3,3], ['x','y','z'])):
+    print(f"color : {colors[i]}    z: {z}    zdir: {zdir}")
+    side = Rectangle((-3, -3), 2, 2, facecolor=colors[i])
+    ax.add_patch(side)
+    art3d.pathpatch_2d_to_3d(side, z=z, zdir=zdir)
+
+plt.show()
